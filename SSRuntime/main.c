38161e33b839891555a4f77e93aa3c0f705682da
francesco <donnini.francesco00@gmail.com>
2026-02-16 19:16:03 +0100

    added pthread_create wrapper

--- SSRuntime/main.c
+++ SSRuntime/main.c
@@ -1,16 +1,25 @@
 #define _GNU_SOURCE
 #include "allocator.h"
+#include <pthread.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <asm/prctl.h>
+#include <dlfcn.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
 #define SS_SIZE  (4096)
 #define SS_BASE  ((void*)0x700000000000)
 #define MEM_SIZE (64ULL * 1024 * 1024 * 1024)
+
+typedef int (*RealPThreadCreate)(pthread_t*, const pthread_attr_t*, void* (*)(void*), void*);
+
+typedef struct {
+    void *(*original_routine)(void *);
+    void *original_arg;
+} PThreadWrapperArgs;
 
 static inline void get_chunk(SSChunk *chunk) {
      if (syscall(SYS_arch_prctl, ARCH_SET_GS, (unsigned long)chunk) != 0) {
@@ -43,4 +52,36 @@ void SSThreadInit(void) {
 
 static inline void put_chunk(SSChunk *chunk) {
     MemPoolRelease(chunk);
+}
+
+static void *thread_trampoline(void *arg) {
+    PThreadWrapperArgs *w_args = (PThreadWrapperArgs *)arg;
+    
+    SSThreadInit();
+
+    void *(*original_routine)(void *) = w_args->original_routine;
+    void *original_data = w_args->original_arg;
+    free(w_args);
+
+    return original_routine(original_data);
+}
+
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg) {
+    static RealPThreadCreate real_pthread_create = NULL;
+    if (!real_pthread_create) {
+        real_pthread_create = (RealPThreadCreate)dlsym(RTLD_NEXT, "pthread_create");
+    }
+
+    PThreadWrapperArgs *wrapper_args = malloc(sizeof(PThreadWrapperArgs));
+    if (!wrapper_args) {
+        return -1; 
+    }
+
+    wrapper_args->original_routine = start_routine;
+    wrapper_args->original_arg = arg;
+    int ret = real_pthread_create(thread, attr, thread_trampoline, wrapper_args);
+    if (ret != 0) {
+        free(wrapper_args);
+    }
+    return ret;
 }
