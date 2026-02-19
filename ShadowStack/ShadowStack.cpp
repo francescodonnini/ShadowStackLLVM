f3f634ff667a659463744d44bb59fca5b4c94aa4
Francesco Donnini <donnini.francesco00@gmail.com>
2026-02-19 14:50:09 +0100

    Replaced CreateAdd with CreateGEP (to increment pointers), replaced return address calculation with LLVM intrinsics: the volatile load from address of return address return value is necessary because the compiler might optimize the second read of the return address away - i.e. it uses the result from the first read of the return address for comparison (in very short function). fixed access on the shadow stack object, now the top of the stack is correctly loaded

--- ShadowStack/ShadowStack.cpp
+++ ShadowStack/ShadowStack.cpp
@@ -31,42 +31,27 @@ void SSFunctionPass::instrumentPreamble(Function &F) {
   auto &EntryBB = F.getEntryBlock();
   IRBuilder<> Builder(&EntryBB, EntryBB.getFirstNonPHIIt());
 
+  auto *M = F.getParent();
   auto *Int64Ty = Builder.getInt64Ty();
   auto *GsPtrTy = PointerType::get(Int64Ty, GS_ADDR_SPACE);
   auto *Int64PtrTy = PointerType::getUnqual(Int64Ty);
 
-  // Push return address at the top of the stack
-  // 1. SSOPtr is the pointer to the shadow stack object stored in gs:0
+  auto *RetAdrIntr = Intrinsic::getOrInsertDeclaration(M, Intrinsic::returnaddress);
+  auto *RetAdrPtr = Builder.CreateCall(RetAdrIntr, {Builder.getInt32(0)});
+  auto *RetAdrVal = Builder.CreatePtrToInt(RetAdrPtr, Int64Ty, "ret_adr_before");
+
+  // 2. Push the return address at the top of the shadow stack
+  // Get the address of the top of the shadow stack
+  // The address is stored as the first element of the shadow stack object
   auto *SSOPtr = Builder.CreateIntToPtr(Builder.getInt64(0), GsPtrTy, "ss_obj_ptr");
+  auto *SSTopFieldPtr = Builder.CreateLoad(Int64PtrTy, SSOPtr);
+  auto *SSTopPtrPtr = Builder.CreateLoad(Int64PtrTy, SSTopFieldPtr);
 
-  // 2. Load the address of the next shadow stack's slot stored in gs:0
-  auto *SSObjAdr = Builder.CreateLoad(Int64Ty, SSOPtr, true, "ss_obj_adr");
-  auto *SSObjPtr = Builder.CreateIntToPtr(SSObjAdr, Int64PtrTy, "ss_obj_ptr");
-  auto *SSPTopVal = Builder.CreateLoad(Int64Ty, SSObjPtr, true, "ss_top");
-
-  // 3. Store the return address to the top of the shadow stack
-  // Read te retun address from the stack
-  // WARNING: Intrinsic::returnaddress might reuse the register populated by the preamble instrumentation to
-  // get the return address so it is useless in this scenario
-  auto *FrameAddrFn = Intrinsic::getOrInsertDeclaration(F.getParent(), Intrinsic::frameaddress, {Int64PtrTy});
-  auto *FramePtr = Builder.CreateCall(FrameAddrFn, {Builder.getInt32(0)});
-
-  // On x86_64, return address is at [rbp + 8]
-  auto *FramePtrAdr = Builder.CreatePtrToInt(FramePtr, Int64Ty);
-  auto *RetAdrPtrVal = Builder.CreateAdd(FramePtrAdr, Builder.getInt64(8), "ss_ret_ptr");
-
-  // Load the return address
-  auto *RetAdrPtr = Builder.CreateIntToPtr(RetAdrPtrVal, Int64PtrTy);
-  auto *RetAdrVal = Builder.CreateLoad(Int64Ty, RetAdrPtr);
-
-  auto *SSPTopPtr = Builder.CreateIntToPtr(SSPTopVal, Int64PtrTy, "ss_top_ptr");
-  Builder.CreateStore(RetAdrVal, SSPTopPtr, true);
+  Builder.CreateStore(RetAdrVal, SSTopPtrPtr);
 
   // 4. Advance the top of the shadow stack
-  auto *NewSSPTopVal = Builder.CreateAdd(SSPTopVal, Builder.getInt64(8), "ss_push");
-
-  // 5. Store the new shadow stack pointer in gs:0
-  Builder.CreateStore(NewSSPTopVal, SSOPtr, true);
+  auto *NewSSTopPtr = Builder.CreateGEP(Int64Ty, SSTopFieldPtr, Builder.getInt64(1), "ss_push");
+  Builder.CreateStore(NewSSTopPtr, SSOPtr);
 }
 
 void SSFunctionPass::instrumentEpilogue(Function &F) {
@@ -85,39 +70,25 @@ void SSFunctionPass::instrumentEpilogue(Function &F) {
 void SSFunctionPass::instrumentRet(Function &F, ReturnInst &I) {
   IRBuilder<> Builder(&I);
 
+  auto *M = F.getParent();
   auto *Int64Ty = Builder.getInt64Ty();
   auto *GsPtrTy = PointerType::get(Int64Ty, GS_ADDR_SPACE);
   auto *Int64PtrTy = PointerType::get(Int64Ty, 0);
 
-  // 1. SSPVar is an HANDLE to gs:0
-  auto *SSOPtr = Builder.CreateIntToPtr(Builder.getInt64(0), GsPtrTy);
+  auto *AdrOfRetAdrIntr = Intrinsic::getOrInsertDeclaration(M, Intrinsic::addressofreturnaddress, { Builder.getPtrTy() });
+  auto *AdrOfRetAdrPtr = Builder.CreateCall(AdrOfRetAdrIntr, {});
+  auto *RetAdrVal = Builder.CreateLoad(Int64Ty, AdrOfRetAdrPtr, true, "ret_adr_after");
 
-  // 2. Load the current top of the shadow stack
-  auto *SSObjAdr = Builder.CreateLoad(Int64Ty, SSOPtr, true, "ss_obj_adr");
-  auto *SSObjPtr = Builder.CreateIntToPtr(SSObjAdr, Int64PtrTy, "ss_obj_ptr");
-  auto *SSPTopVal = Builder.CreateLoad(Int64Ty, SSObjPtr, true, "ss_top");
-  
-  // 3. Decrement the current stack pointer and update the shadow stack pointer
-  auto *NewSSPTopVal = Builder.CreateSub(SSPTopVal, Builder.getInt64(8), "ss_pop");
-  Builder.CreateStore(NewSSPTopVal, SSOPtr, true);
-
-  // 5. Read the return address from the top of the shadow stack
-  auto *NewSSPTopPtr = Builder.CreateIntToPtr(NewSSPTopVal, Int64PtrTy);
-  auto *ShadowRetVal = Builder.CreateLoad(Int64Ty, NewSSPTopPtr, true);
-
-  // Read te retun address from the stack
-  // WARNING: Intrinsic::returnaddress might reuse the register populated by the preamble instrumentation to
-  // get the return address so it is useless in this scenario
-  auto *FrameAddrFn = Intrinsic::getOrInsertDeclaration(I.getModule(), Intrinsic::frameaddress, {Int64PtrTy});
-  auto *FramePtr = Builder.CreateCall(FrameAddrFn, {Builder.getInt32(0)});
-
-  // On x86_64, return address is at [rbp + 8]
-  auto *FramePtrAdr = Builder.CreatePtrToInt(FramePtr, Int64Ty);
-  auto *RetAdrPtrVal = Builder.CreateAdd(FramePtrAdr, Builder.getInt64(8), "ss_ret_ptr");
-
-  // Load the return address
-  auto *RetAdrPtr = Builder.CreateIntToPtr(RetAdrPtrVal, Int64PtrTy);
-  auto *RetAdrVal = Builder.CreateLoad(Int64Ty, RetAdrPtr);
+  // 2. Pop the return address from the top of the shadow stack
+  // Get the address of the top of the shadow stack
+  // The address is stored as the first element of the shadow stack object
+  auto *SSOPtr = Builder.CreateIntToPtr(Builder.getInt64(0), GsPtrTy, "ss_obj_ptr");
+  auto *SSTopFieldPtr = Builder.CreateLoad(Int64PtrTy, SSOPtr);
+  auto *SSTopPtr = Builder.CreateLoad(Int64PtrTy, SSTopFieldPtr);
+  auto *SSCurPtr = Builder.CreateGEP(Int64Ty, SSTopPtr, Builder.getInt64(-1), "ss_curr_ptr");
+  auto *ShadowRetVal = Builder.CreateLoad(Int64Ty, SSCurPtr);
+  // Update the top of the shadow stack
+  Builder.CreateStore(SSCurPtr, SSOPtr);
   
   // Compare return address
   auto *Compare = Builder.CreateICmpNE(ShadowRetVal, RetAdrVal);
@@ -129,7 +100,7 @@ void SSFunctionPass::instrumentRet(Function &F, ReturnInst &I) {
   // Fail path
   auto *FailBB = BasicBlock::Create(F.getContext(), "ss_fail", &F);
   IRBuilder<> FailPathBuilder(FailBB);
-  auto *Trap = Intrinsic::getOrInsertDeclaration(F.getParent(), Intrinsic::trap);
+  auto *Trap = Intrinsic::getOrInsertDeclaration(M, Intrinsic::trap);
   FailPathBuilder.CreateCall(Trap);
   FailPathBuilder.CreateUnreachable();
 
